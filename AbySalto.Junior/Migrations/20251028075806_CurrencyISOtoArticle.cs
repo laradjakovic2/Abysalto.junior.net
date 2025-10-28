bdcd3fbfaeb9d174efeff11b22c599b0804ad459
Lara Đaković <lara.dakovic@zeraxo.com>
2025-10-28 09:09:43 +0100

    currency iso to article migratino

---
+++ AbySalto.Junior/Migrations/20251028075806_CurrencyISOtoArticle.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AbySalto.Junior.Migrations
+{
+    /// <inheritdoc />
+    public partial class CurrencyISOtoArticle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CurrencyISO",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrencyISO",
+                table: "Articles");
+        }
+    }
+}
