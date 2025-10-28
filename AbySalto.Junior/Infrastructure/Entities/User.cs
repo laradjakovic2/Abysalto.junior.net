6907e4eeee59ad23a8fb81b05998ee027184015f
Lara Đaković <lara.dakovic@zeraxo.com>
2025-10-28 08:42:12 +0100

    update user migration

--- AbySalto.Junior/Infrastructure/Entities/User.cs
+++ AbySalto.Junior/Infrastructure/Entities/User.cs
@@ -3,8 +3,9 @@
     public class User
     {
         public int Id { get; set; }
-        public int Name { get; set; }
-        public int Surname { get; set; }
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public required string Email { get; set; }
 
         public ICollection<Order> Orders { get; set; } = [];
 
---
+++ AbySalto.Junior/Migrations/20251028074053_UserUpdate.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AbySalto.Junior.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Surname",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
